186e295e3b6614150c4afb11199089b15af1c682
Elvan H <elvanhd@gmail.com>
2021-12-28 16:56:38 +0700

    fetching and displaying a list of authors

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,17 +1,28 @@
 const express = require('express');
 
+const db = require('../data/database');
+
 const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
+router.get('/posts', (req, res) => {
   res.render('posts-list');
 });
 
-router.get('/new-post', async function (req, res) {
-  res.render('create-post');
+router.get('/new-post', async (req, res) => {
+  db.getDb()
+    .collection('authors')
+    .find({})
+    .toArray((err, authors) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.render('create-post', { authors });
+    });
 });
 
 module.exports = router;
