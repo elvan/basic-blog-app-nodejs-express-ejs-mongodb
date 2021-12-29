945c1d9eb223955cb2a6a436cf72410296aad1cf
Elvan H <elvanhd@gmail.com>
2021-12-30 04:49:49 +0700

    updating the dom based on the response

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,4 +1,24 @@
+// @ts-nocheck
 const loadCommentsButtonEl = document.getElementById('load-comments-button');
+const commentsSectionEl = document.getElementById('comments');
+
+function createCommentsList(comments) {
+  const commentsListEl = document.createElement('ul');
+
+  for (const comment of comments) {
+    const commentEl = document.createElement('li');
+    commentEl.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+
+    commentsListEl.appendChild(commentEl);
+  }
+
+  return commentsListEl;
+}
 
 async function fetchCommentsForPost() {
   const postId = loadCommentsButtonEl?.dataset.postid;
@@ -9,7 +29,10 @@ async function fetchCommentsForPost() {
   }
 
   const comments = await response.json();
-  console.log(comments);
+
+  const commentsListEl = createCommentsList(comments);
+  commentsSectionEl.innerHTML = '';
+  commentsSectionEl.appendChild(commentsListEl);
 }
 
 loadCommentsButtonEl?.addEventListener('click', fetchCommentsForPost);
